5b3a1c9346c975fe244b698b98ae39d58592b720
OmNomLettuce <uyenhuynhtrang@gmail.com>
2025-05-15 12:04:01 -0500

    Update pong-classes.js

--- javascript/pong-classes.js
+++ javascript/pong-classes.js
@@ -21,7 +21,7 @@ export class Game {
     this.mousePosition = { x: 0, y: 0 };
     this.mute = false; //default mute state
     this.volume = 10; //default volume output 0-10
-    this.computerState = 3; //default computer control amount //0 or 1 is easy //2 medium // 4 is impossible
+    this.computerState = 3; //default computer control amount //0 off //1 easy //2 medium //3 hard //4 impossible
     this.computerDifficulty = ["off", "easy", "medium", "hard", "impossible"]; //sets the computer difficulty names
     this.computerDifficultySpeed = [0, 2, 4, 8, 100]; //sets the computer difficulty speeds
   }
@@ -41,9 +41,9 @@ export class Ball {
     this.size = { x: 20, y: 20 }; // default ball size
     this.position = { x: 50, y: 50 }; // default ball position
     this.direction = { x: 2, y: 0 }; //Ball speed when game starts
-    this.acceleration = 1.25; //X speed multipler on successful block
+    this.acceleration = 1.2; //X speed multipler on successful block
     this.initial = { x: 2, y: 0 }; //speed reset on point score
-    this.maximum = { x: 50, y: 50 }; //maximum speed
+    this.maximum = { x: 40, y: 40 }; //maximum speed
     this.inertialTransfer = 0.25; //multiplier for velocity.y transfer from paddle
     this.htmlElement = null; //element on HTML page
   }
@@ -63,9 +63,9 @@ export class Paddle {
     this.size = { x: 10, y: 75 }; //default paddle size
     this.position = { x: 50, y: 50 }; //paddle position, (Y is top)
     this.oldPosition = { x: 50, y: 50 }; //paddle position 1 frame ago
-    this.velocity = { x: 0, y: 0.5 }; //calculated velocity
+    this.velocity = { x: 0.75, y: 0.75 }; //calculated velocity
     this.direction = { x: 0, y: 0 }; //position offset
-    this.speed = 10; //rate that paddle moves with keyboard
+    this.speed = 10.5; //rate that paddle moves with keyboard
     this.hasHit = true; //keep track of wheter paddle hit ball or not
     this.htmlElement = null; //element on HTML page
   }
